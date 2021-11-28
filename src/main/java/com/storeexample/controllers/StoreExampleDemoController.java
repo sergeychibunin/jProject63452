edf30547326df486c67fa24273a6582dc796dc90
Sergey Chibunin <chibunin1988@gmail.com>
2021-11-28 09:48:57 +0300

    Fix a DB config. Add a web controller

---
+++ src/main/java/com/storeexample/controllers/StoreExampleDemoController.java
@@ -0,0 +1,18 @@
+package com.storeexample.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StoreExampleDemoController {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @GetMapping(value = "/")
+    public String getHomePage() {
+        return "index.html";
+    }
+}
