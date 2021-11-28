8a12876cb916bc84895952b59ec12d4d791db926
Sergey Chibunin <chibunin1988@gmail.com>
2021-11-28 18:45:45 +0300

    Add RSSFeed model

---
+++ src/main/java/com/storeexample/demo/persistence/model/RSSFeed.java
@@ -0,0 +1,29 @@
+package com.storeexample.demo.persistence.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class RSSFeed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    public RSSFeed() {
+
+    }
+
+    public RSSFeed(String name) {
+        this.name = name;
+    }
+}
