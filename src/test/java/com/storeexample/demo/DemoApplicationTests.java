e9f47057334a87950488171e73b7f6d2147d2cc6
Sergey Chibunin <chibunin1988@gmail.com>
2021-12-02 07:33:13 +0300

    Add test for a web controller

--- src/test/java/com/storeexample/demo/DemoApplicationTests.java
+++ src/test/java/com/storeexample/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
 package com.storeexample.demo;
 
+import com.storeexample.demo.controllers.StoreExampleDemoController;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	private StoreExampleDemoController storeExampleDemoController;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(storeExampleDemoController);
 	}
 
 }
