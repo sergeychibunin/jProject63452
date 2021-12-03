8ac477196c7f062f98cbd0e012661310ef35c096
Sergey Chibunin <chibunin1988@gmail.com>
2021-12-03 21:52:25 +0300

    Add test for the home page. WIP

--- src/test/java/com/storeexample/demo/DemoApplicationTests.java
+++ src/test/java/com/storeexample/demo/DemoApplicationTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -29,6 +31,13 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 		Assertions.assertNotNull(storeExampleDemoController);
+	}
+
+	@Test
+	public void getHomePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+
 	}
 
 }
