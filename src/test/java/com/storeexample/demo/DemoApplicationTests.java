86ffe4c8e4f2fccd21292c7d093b8a9510835819
Sergey Chibunin <chibunin1988@gmail.com>
2021-12-04 08:03:44 +0300

    Add test for the home page.

--- src/test/java/com/storeexample/demo/DemoApplicationTests.java
+++ src/test/java/com/storeexample/demo/DemoApplicationTests.java
@@ -1,32 +1,24 @@
 package com.storeexample.demo;
 
 import com.storeexample.demo.controllers.StoreExampleDemoController;
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	@Autowired
 	private StoreExampleDemoController storeExampleDemoController;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
 
 	@Test
 	void contextLoads() {
