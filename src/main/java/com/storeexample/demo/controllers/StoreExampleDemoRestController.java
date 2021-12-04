00a97c5cdb3c594e2926a9c0f2bf7dbe1bfb752c
Sergey Chibunin <chibunin1988@gmail.com>
2021-12-04 10:43:40 +0300

    Add REST API for getting all the RSS Feeds

--- src/main/java/com/storeexample/demo/controllers/StoreExampleDemoRestController.java
+++ src/main/java/com/storeexample/demo/controllers/StoreExampleDemoRestController.java
@@ -1,20 +1,32 @@
 package com.storeexample.demo.controllers;
 
+import com.storeexample.demo.persistence.dao.services.interfaces.RSSFeedService;
+import com.storeexample.demo.persistence.model.RSSFeed;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class StoreExampleDemoRestController {
 
+    @Autowired
+    private RSSFeedService rssFeedService;
+
     @GetMapping("/healthcheck")
     public Map<String, String> checkHealth() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "ok");
         return response;
+    }
+
+    @GetMapping("/rssfeeds")
+    public List<RSSFeed> getAllRssFeeds() {
+        return rssFeedService.listAllRSSFeeds();
     }
 }
